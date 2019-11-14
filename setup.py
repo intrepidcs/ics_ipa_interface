bd19c0fe2fa1af6bd7bcda55b3342fc4ed256ed3
Zaid Nackasha <znackasha@intrepidcs.com>
2019-11-14 18:39:35 -0500

    added pypi support

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
+import os
+import pathlib
 
 VERSION = '1.0.0'
 
+with open(os.path.json(pathlib.Path(__file__).parent, "README.md"), "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ics_ipa_interface',
-    packages=['src'],
+    packages=['ics_ipa_interface'],
     version=VERSION,
     description='API used to to allow users to make scripts that work local'
                 ' and on wivi server',
-    long_description='This project is designed to allow users of IPA to seamlessly \
-                      transition between Desktop and WiVi.',
+    long_description=long_description,
     maintainer='Zaid Nackasha',
     maintainer_email='ZNackasha@intrepidcs.com',
     url='https://github.com/intrepidcs/ics_ipa_interface',
